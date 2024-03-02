1b56cbd4c969b6d3e4d7be105df439b5d925e6b0
DanielBraga04 <90907880+DanielBraga04@users.noreply.github.com>
2024-03-02 15:20:08 -0300

    Update coords-ROI.py

--- OpenCV_Python/coords-ROI.py
+++ OpenCV_Python/coords-ROI.py
@@ -1,28 +1,5 @@
 import cv2
 
-clicked_points = []
-
-def Capture_Event(event, x, y, flags, params):
-    global clicked_points
-
-    if event == cv2.EVENT_LBUTTONDOWN:
-        #clicked_points.append((x, y))
-
-        if len(clicked_points) == 2:
-            # Mostra as coordenadas dos dois cliques
-            print("Coordenadas do primeiro clique:", clicked_points[0])
-            print("Coordenadas do segundo clique:", clicked_points[1])
-            print("")
-
-            #cv2.rectangle(obj_img, clicked_points[0], clicked_points[1], (0, 0, 255), 2)
-
-            #cv2.imshow("Image", obj_img)
-
-            # Limpa a lista para que novos cliques possam ser registrados
-            clicked_points.clear()
-
-        #print(f"({x}, {y})")
-
 def showImage(img):
     global obj_img
 
