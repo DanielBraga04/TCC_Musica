79a701820fcf8d2b1cd905bf09f6bcc912dc27bb
DanielBraga04 <90907880+DanielBraga04@users.noreply.github.com>
2024-03-02 15:12:09 -0300

    Add files via upload

---
+++ OpenCV_Python/coords-ROI.py
@@ -0,0 +1,52 @@
+import cv2
+
+clicked_points = []
+
+def Capture_Event(event, x, y, flags, params):
+    global clicked_points
+
+    if event == cv2.EVENT_LBUTTONDOWN:
+        #clicked_points.append((x, y))
+
+        if len(clicked_points) == 2:
+            # Mostra as coordenadas dos dois cliques
+            print("Coordenadas do primeiro clique:", clicked_points[0])
+            print("Coordenadas do segundo clique:", clicked_points[1])
+            print("")
+
+            #cv2.rectangle(obj_img, clicked_points[0], clicked_points[1], (0, 0, 255), 2)
+
+            #cv2.imshow("Image", obj_img)
+
+            # Limpa a lista para que novos cliques possam ser registrados
+            clicked_points.clear()
+
+        #print(f"({x}, {y})")
+
+def showImage(img):
+    global obj_img
+
+    obj_img = cv2.imread(img)
+
+    #ROI
+    roi = cv2.selectROI(obj_img)
+    print(roi)
+    im_cropped = obj_img[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
+
+    #cv2.namedWindow("Image")
+    #cv2.setMouseCallback("Image", Capture_Event)
+    #cv2.imshow("Image", obj_img)
+
+    #MOSTRAR ROI
+    cv2.imshow("Cropped Image", im_cropped)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def main():
+    img = "./img/partitura.png"
+    showImage(img)
+    
+
+if __name__ == "__main__":
+    main()
