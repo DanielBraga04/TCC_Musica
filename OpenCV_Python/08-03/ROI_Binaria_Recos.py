c4be4c33b4f1d4a1254c9d18131a6af0b2395a6b
DanielBraga04 <90907880+DanielBraga04@users.noreply.github.com>
2024-03-08 14:55:29 -0300

    Add files via upload

---
+++ OpenCV_Python/08-03/ROI_Binaria_Recos.py
@@ -0,0 +1,39 @@
+import cv2
+import numpy as np
+
+def showImage(img):
+    global obj_img
+
+    obj_img = cv2.imread(img)
+
+    # ROI
+    roi = cv2.selectROI(obj_img)
+    print(roi)
+    im_cropped = obj_img[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
+
+    # Mostrar ROI
+    cv2.imshow("Cropped Image", im_cropped)
+
+    # Converter a imagem para escala de cinza
+    gray_im_cropped = cv2.cvtColor(im_cropped, cv2.COLOR_BGR2GRAY)
+
+    # Binarizar a imagem (pixels brancos tornam-se 1, pixels pretos tornam-se 0)
+    binary_array = np.where(gray_im_cropped > 128, 1, 0)
+
+    # Converter o array binário de volta para uma imagem
+    reconstructed_image = np.zeros_like(gray_im_cropped, dtype=np.uint8)
+    reconstructed_image[binary_array == 1] = 255
+
+    # Mostrar a imagem binarizada e reconstruída
+    cv2.imshow("Binary Image", reconstructed_image)
+    print(binary_array)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+def main():
+    img = "./img/partitura.png"
+    showImage(img)
+
+if __name__ == "__main__":
+    main()
---
+++ OpenCV_Python/08-03/ROI_MBR.py
@@ -0,0 +1,55 @@
+import cv2
+import numpy as np
+
+def showImage(img):
+
+    obj_img = cv2.imread(img)
+
+    #ROI
+    roi = cv2.selectROI(obj_img)
+    print(roi)
+    im_cropped = obj_img[int(roi[1]):int(roi[1]+roi[3]), int(roi[0]):int(roi[0]+roi[2])]
+
+    # Converte a área selecionada para escala de cinza
+    gray_area = cv2.cvtColor(im_cropped, cv2.COLOR_BGR2GRAY)
+
+    # Cria um array binário onde 0 é branco e 1 é preto
+    binary_array = np.where(gray_area == 255, 1, 0)
+
+    print(binary_array)
+
+    reconstruir(binary_array)
+
+    #MOSTRAR ROI
+    #cv2.imshow("Cropped Image", im_cropped)
+
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def reconstruir(binario):
+
+    largura, altura = binario.shape
+    
+    print("Dimensões para a reconstrução: " + str(largura) + "x" + str(altura))
+
+    reconstructed_image = np.zeros((largura, altura, 3), dtype=np.uint8)
+    reconstructed_image[:, :, 0] = binario * 255  # Canal azul
+    reconstructed_image[:, :, 1] = binario * 255  # Canal verde
+    reconstructed_image[:, :, 2] = binario * 255  # Canal vermelho
+
+    #print(reconstructed_image)
+
+    cv2.imshow("Reconstructed Image", reconstructed_image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+#FUNÇÃO PRINCIPAL
+def main():
+    img = "./img/partitura.png"
+    showImage(img)
+    
+
+if __name__ == "__main__":
+    main()
